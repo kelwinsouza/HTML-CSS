88a5d221fe5f5db6a2a466bea5bbe7b72c995328
kelwin souza santos <kelwin29sk8@gmail.com>
2025-01-24 21:43:40 -0300

    att

--- javascript.ORIGAMID/ex001.js/script.js
+++ javascript.ORIGAMID/ex001.js/script.js
@@ -1,1 +1,19 @@
-console.log('oi');
+// Declarar uma variável com o seu nome
+var nome = 'kelwin'
+// Declarar uma variável com a sua idade
+var idade = '23'
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'lasanha';
+
+// Declarar 5 variáveis diferentes sem valores
+var time ='gremio', 
+cidade = 'porto alegre', 
+pais = 'brasil', 
+endereco ='Rua: são jaoquin 548',
+ cep = '99999999 não sei';
+
+console.log(nome, idade , comidaFavorita , time , cidade , pais , endereco , cep);
