fccf65009117ab75a3415f9cfa7b9f41bcddaf75
kelwin souza santos <kelwin29sk8@gmail.com>
2025-01-27 21:49:33 -0300

    tamplete

---
+++ javascript.ORIGAMID/ex002.js/script.js
@@ -0,0 +1,54 @@
+var nome = 'André';
+console.log(typeof nome);
+
+var idade = 28;
+var time =null;
+console.log(typeof time);
+
+var nome = 'André';
+var sobrenome = 'Rafael';
+var nomeCompleto = nome + ' ' +sobrenome;
+console.log(nomeCompleto);
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+console.log(typeof frase);
+
+var ano = 2018;
+var mes = 8;
+console.log(typeof ano + mes);
+
+
+var frase1 = 'teste'
+var frase2 = "Esse é o \"melhor\" jogo";
+console. log(frase1, frase2);
+
+ var gols = 1000;
+ var frase1 = 'Romário fez ' + gols + ' gols';
+ var frase2 = `Romário fez ${gols * 3} gols`;
+ console.log(frase2);
+
+ // Declare uma variável contendo uma string
+var nome = 'Kelwin';
+console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var idade = '28';
+console.log( typeof idade);
+
+// Declare uma variável com a sua idade
+var idade = 28;
+console.log(idade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var sobreNome = 'Souza Santos';
+var nomeCompleto = `${nome} ${sobreNome}`;
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
+
+
---
+++ javascript.ORIGAMID/ex003js/script.js
@@ -0,0 +1,3 @@
+var idade = 2e10;
+
+console.log(idade); 
