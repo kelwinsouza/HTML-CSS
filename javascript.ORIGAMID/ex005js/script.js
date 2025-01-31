090008d08d5c519d2fba07d212b66e6b498ead7e
kelwin souza santos <kelwin29sk8@gmail.com>
2025-01-30 21:43:35 -0300

    att

---
+++ javascript.ORIGAMID/ex005js/script.js
@@ -0,0 +1,19 @@
+function areaQuadrado (lado) {
+    return lado * lado;
+}
+
+console.log (areaQuadrado(10));
+
+function pi(){
+ return 3.14;
+}
+var total = 6 * pi ();
+
+console.log (pi());
+
+function imc(peso, altura){
+    var imc = peso / (altura * altura);
+    return imc;
+}
+console.log (imc(75, 1.73));
+
