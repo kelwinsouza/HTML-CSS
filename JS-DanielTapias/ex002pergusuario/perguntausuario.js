7d188ad47e54bc6617eb0fab3e60348fa502382f
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-05 20:36:45 -0300

    attt

--- JS-DanielTapias/ex002pergusuario/perguntausuario.js
+++ JS-DanielTapias/ex002pergusuario/perguntausuario.js
@@ -1,2 +1,8 @@
 //const nomeDoUsuario = prompt("Escreva seu nome");
 //alert( "olá, " + nomeDoUsuario + ", bem vindo");
+
+let numero1 = prompt('Digite um número')
+let numero2 = prompt("Digite outro número")
+let somaDosNumeros = parseFloat( numero1) + Number(numero2);
+alert("A soma dos dois números é, " + somaDosNumeros);
+ 
