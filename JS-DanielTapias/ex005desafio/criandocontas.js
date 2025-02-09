3a1b2505872fe80bbd0d51e917abc4d1f6b86ea1
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-09 19:35:21 -0300

    att

--- JS-DanielTapias/ex005desafio/criandocontas.js
+++ JS-DanielTapias/ex005desafio/criandocontas.js
@@ -10,14 +10,17 @@ x *= 5;
 ++x;
 //sem alterar o valor da variavel, mostre na tela o resto da divisão por 6
 //a mensagem deve ser: o resto da divisão de (variavel) por 6 é o (resultado)
-x = ("o resto da divisão de x por 6 é o  " + (x / 6) );
+alert("O resto da divisão de " + x + " por 6 é " + x % 6)
 
 //sem alterar o valor da variavel, mostre no resto da tela a divisão por 6 elevado ao quadrado
 //a mensagem deve ser: O quadrado do resto da divisão de (variavel) por 6 é (resultado)
 
+alert("O quadrado do resto da divisão de " + x + " por 6 é " + ( (x % 6) ** 2) )
 
 //mostre na tela o valor da variavel 
+alert(x)
 
 //agora divida o valor da variavel por 4 e retribua o valor 
-
+x /= 4
 //qual o valor da variavel ?
+alert(x)
