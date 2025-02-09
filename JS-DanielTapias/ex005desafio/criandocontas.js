cc5f7ef94436aa84c41e34bb22c5be1ab30bfe6e
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-09 19:22:35 -0300

    continhas

---
+++ JS-DanielTapias/ex005desafio/criandocontas.js
@@ -0,0 +1,23 @@
+// ATENÇÃO: PARA ESSE ECERCÍCIO VOCÊ PODE CRIAR APENAS UMA ÚNICA VARIAVEL
+
+//Crie uma variavel e atribua o valor 10
+let x = 10;
+//some 5 a essa variavel e retribua o valor
+x += 5;
+//multiplica o valor da variavel por 5 e retribua o valor
+x *= 5;
+//some 1 a variavel e retribua o valor
+++x;
+//sem alterar o valor da variavel, mostre na tela o resto da divisão por 6
+//a mensagem deve ser: o resto da divisão de (variavel) por 6 é o (resultado)
+x = ("o resto da divisão de x por 6 é o  " + (x / 6) );
+
+//sem alterar o valor da variavel, mostre no resto da tela a divisão por 6 elevado ao quadrado
+//a mensagem deve ser: O quadrado do resto da divisão de (variavel) por 6 é (resultado)
+
+
+//mostre na tela o valor da variavel 
+
+//agora divida o valor da variavel por 4 e retribua o valor 
+
+//qual o valor da variavel ?
