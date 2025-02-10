7acd511e5caa100bc0d704dd06d222c41a85d814
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-09 22:04:41 -0300

    att

---
+++ JS-DanielTapias/ex007Boolean, Number e String - Typeof/NaNindefirido.js
@@ -0,0 +1,33 @@
+let mensagem = "olá euu sou uma mensagem de 'texto'" 
+console.log(mensagem);
+
+console.log( typeof 0)
+console.log(typeof "0")
+console.log(typeof String(0))
+
+console.log(Boolean("0"));//true
+console.log(Boolean(0));//false
+console.log(Boolean(undefined));//false
+console.log(Boolean(null));//false
+console.log(Boolean(""));//false
+
+
+let naoDeclarado =null
+console.log(naoDeclarado)
+
+naoDeclarado = 0
+console.log(naoDeclarado)
+
+
+let n =parseFloat("a")
+console.log(n)
+console.log(typeof n)
+console.log("--------------------------")
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof 0)
+console.log(typeof "")
+console.log(typeof true)
+
+
