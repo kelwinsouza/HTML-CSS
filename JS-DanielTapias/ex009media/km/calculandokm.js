a2ee18cf24f562c7e7ee5f3fdae4a20eae868049
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-18 20:41:17 -0300

    att

--- JS-DanielTapias/ex009media/km/calculandokm.js
+++ JS-DanielTapias/ex009media/km/calculandokm.js
@@ -1,13 +1,15 @@
 let cidades = {
-    "porto alegre": 0,
-    "glorinha":45,
-    "caraa":90
+    "porto alegre": {"glorinha": 55, "caraa": 95, "canoas": 21},
+    "glorinha": {"porto alegre": 55, "caraa": 40, "canoas": 54},
+    "caraa": {"porto alegre": 95, "glorinha": 40, "canoas": 85},
+    "canoas": {"porto alegre": 21, "glorinha": 54, "caraa": 85}
 };
 
+function  calcularDistancia(){
 let cidade1 = prompt("Digite a primeira cidade:").toLowerCase();
 let cidade2 = prompt("Digite a segunda cidade:").toLowerCase();
 
-let distancia = Math.abs(cidades[cidade1] - cidades[cidade2]);
+let distancia = cidades[cidade1][cidade2];
 
-alert("A distancia entre " + cidade1 + " e " + cidade2 + "é de  " + distancia + "km.")
+alert("A distancia entre " + cidade1 + " e " + cidade2 + "é de  " + distancia + "km.")}
 
