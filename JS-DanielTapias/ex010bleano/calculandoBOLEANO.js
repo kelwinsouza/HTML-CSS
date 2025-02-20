f0e6a6ad73a4136395f6ad5e7a0b9afed064a68e
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-19 21:03:08 -0300

    att

--- JS-DanielTapias/ex010bleano/calculandoBOLEANO.js
+++ JS-DanielTapias/ex010bleano/calculandoBOLEANO.js
@@ -1,6 +1,7 @@
+function calcular (){
 let n1 = parseFloat(prompt("digite um número de 0 a 10"));
 let n2 = parseFloat(prompt("digite outro número de 0 a 10"))
 
 let media = (n1 + n2) / 2;
 
-alert("está aprovado?" + (media > 4 && n1 > 0 && n2 > 0 ));
+alert("está aprovado? " + (media > 4 && n1 > 0 && n2 > 0 ));}
