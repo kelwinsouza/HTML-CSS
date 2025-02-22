a8f3c5bb696405ac462d9361337434f14f6455a2
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-21 21:06:33 -0300

    att

--- JS-DanielTapias/ex014ifEelse2/ifEelsee.js
+++ JS-DanielTapias/ex014ifEelse2/ifEelsee.js
@@ -2,18 +2,9 @@ function calcular(){
     let n1 = parseFloat(prompt("digite um número:"))
     let n2 = parseFloat(prompt("digite outro número:"))
     
-    //let media = (n1 && n2) 
-    
     if(n1 <= 10 && n2 <= 10 && n1 >=0 && n2 >= 0) {
-        
-        let media = (n1 + n2) / 2
-        if(media > 4 && n1 > 0 && n2 > 0){
-
-            alert("aprovado")
-        }else {
-            alert("reprovadooo")
-        }
-    } else  {
-        alert("Digite apenas números Entre 10")
+        alert("APROVADO!")
+    }else  {
+        alert("Digite apenas números Entre 0 e 10")
     }
-    }
+}
