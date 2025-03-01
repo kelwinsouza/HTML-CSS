79f3f759f2acbcb1054782be99a6ca252c87bfdd
kelwin souza santos <kelwin29sk8@gmail.com>
2025-02-28 22:15:15 -0300

    att

---
+++ ASIMOV/if_else.py
@@ -0,0 +1,9 @@
+idade = 18
+possui_carteira = True
+
+if (idade >= 18) and possui_carteira:
+    print ("Você pode dirigir")
+elif possui_carteira:
+        print("você apenas possui carteira")
+else:
+    print ("você não pode dirigir")
