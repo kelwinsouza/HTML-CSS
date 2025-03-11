556f5993aca035fb87fdc398299af73c9a5faf94
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-11 20:42:39 -0300

    att

--- JS-DanielTapias/ex015IFELSE3/ifEelsee.js
+++ JS-DanielTapias/ex015IFELSE3/ifEelsee.js
@@ -1,4 +1,4 @@
-let n1 = parseFloat(prompt("digite um numero de 0 a 10"))
+/*let n1 = parseFloat(prompt("digite um numero de 0 a 10"))
 let n2 = parseFloat(prompt("digite um núero de 0 a 10"))
 
 if (isNaN(n1) || isNaN(n2)) {
@@ -12,21 +12,21 @@ else {
     alert("digite apenas numeros de 0 a 10")
 }
 
-/*function calcular(){
-    let n1 = parseFloat(prompt("digite um número:"))
-    let n2 = parseFloat(prompt ("digite outro número:") )
-    
-   if (isNaN(n1) || isNaN(n2)){
-    alert("digite apenas números")
-   }
-  else if (n1 <= 10 && n2 <= 10 && n1 >= 0 && n2 >= 0)
-          (n1 <= 10 && n2 <= 10 && n1 >= 0 && n2 >= 0)
+
+   }*/
+    let n1 = parseFloat(prompt("digite um numero de 0 a 10"))
+    let n2 = parseFloat(prompt("digite um núero de 0 a 10"))
+
+  if (n1 <= 10 && n2 <= 10 && n1 >= 0 && n2 >= 0 && isNaN(n1) && isNaN(n2))
+          
  {
          alert("APROVADO!")
     }else  {
         alert("Digite apenas números Entre 0 e 10")
     }
-}
+
 
 //isNan(parseInt(a)) vai ser true
-//isFinite(parseInt(a)) vai ser false*/
+//!isNan(parseInt(a)) vai ser false
+
+//isFinite(parseInt(a)) vai ser false
