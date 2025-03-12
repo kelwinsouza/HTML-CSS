bc060936ebc6ae5e2179e1ff1812f927135debfe
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-11 22:35:56 -0300

    dois exercicios

--- JS-DanielTapias/ex016switch/switch.js
+++ JS-DanielTapias/ex016switch/switch.js
@@ -1,1 +1,78 @@
-let = sema
+/*let n = prompt("digite um número de 0 a 6");
+
+
+switch(n){
+    case "0":
+        alert("domingo")
+        break
+    case "1":
+        alert("segunda-feira")
+        break
+
+    case "2":
+        alert("terça-feira")
+        break
+
+    case "3":
+        alert("quarta-feira")
+        break
+
+    case "4":
+        alert("quinta-feira")
+        break
+  
+    case "5":
+        alert("sexta-feira")  
+        break
+
+    case "6":
+        alert("sábado")
+        break
+
+    default:
+        alert("digite numeros de 0 a 6")
+}
+ let x = 10
+ switch(x){
+    case "10":
+        alert("x é string")
+        
+    case 10:
+        alert("x é number")
+        break
+ }
+
+
+
+if(n === "0"){
+    alert("domingo")
+} else if(n === "1"){
+    alert("segunda")
+} else if(n === "2"){
+    alert("terça")
+} 
+    else {
+        alert("digite numeros de 0 a 2")
+    }
+*/
+
+//let k = (prompt("digite um valor"))
+
+let myVar =  true
+switch(typeof myVar){
+    case "number":
+        alert("myVar é número");
+        break
+    case "string":
+        alert("myVar é texto")
+        break
+    case "boolean":
+        alert("myVar é boolean")
+        break
+    case "undefined":
+        alert("myVar é undefined")
+        break
+        default:
+            alert("nenhuma string conhecido")
+
+}
