c215e9c951b40a9b77ccd31680f7e04480243b4b
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-11 23:24:43 -0300

    loop infinito

---
+++ JS-DanielTapias/ex018loop ou laços de repetição/switch.js
@@ -0,0 +1,25 @@
+//let n = parseFloat(prompt("digite um número"))
+/*let n = 5
+document.write(n + " x 0  =  " + n * 0 + "<br>")
+document.write(n + " x 1  =  " + n * 1 + "<br>")
+document.write(n + " x 2  =   " + n * 2 + "<br>")
+document.write(n + " x 3  =   " + n * 3 + "<br>")
+document.write(n + " x 4  =   " + n * 4 + "<br>")
+document.write(n + " x 5  =   " + n * 5 + "<br>")
+document.write(n + " x 6  =   " + n * 6 + "<br>")
+só fiz isso pra testar*/
+
+let n = parseFloat(prompt("digite um número"))
+let contador = 0
+
+while (contador <= 1000){
+    document.write  ((isNaN(n)alert("digite apenas números")) || (n + " x "  + contador + " = " + n * contador + "<br>"))
+    contador++
+
+}
+
+document.write("saiu do loop")
+
+//em quanto o contador <= a 1000
+//   faça isso
+
