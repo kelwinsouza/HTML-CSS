785d8d9d5035a7081515c13c5cfcdcf65806cdd5
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-12 21:03:20 -0300

    loop while e for

--- JS-DanielTapias/ex018loop while laços de repetição/loopWHILE.js
+++ JS-DanielTapias/ex018loop while laços de repetição/loopWHILE.js
@@ -7,19 +7,36 @@ document.write(n + " x 3  =   " + n * 3 + "<br>")
 document.write(n + " x 4  =   " + n * 4 + "<br>")
 document.write(n + " x 5  =   " + n * 5 + "<br>")
 document.write(n + " x 6  =   " + n * 6 + "<br>")
-só fiz isso pra testar*/
+só fiz isso pra testar
+((isNaN(n)alert("digite apenas números")) ||
+
 
 let n = parseFloat(prompt("digite um número"))
 let contador = 0
 
 while (contador <= 1000){
-    document.write  ((isNaN(n)alert("digite apenas números")) || (n + " x "  + contador + " = " + n * contador + "<br>"))
+    document.write  (n + " x "  + contador + " = " + n * contador + "<br>")
     contador++
 
 }
 
-document.write("saiu do loop")
+document.write("saiu do loop")*/
 
 //em quanto o contador <= a 1000
 //   faça isso
+
+let n = parseFloat(prompt("digite um número"))
+
+    if(!isNaN(n)){
+        
+        let contador = 0
+        while ( contador <= 1000){
+            document.write (n + "x" + contador + "=" + contador + "<br>")
+        contador++
+        }
+        document.write("saiu do programa")
+
+    }   else {
+        alert("digite apenas número")
+    }
 
