15e1577fec54b1fe081546a853cbd7253c8ba734
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-13 21:31:18 -0300

    contado_em_pares
    aqui fiz dois trabalho : fazendo uma linha a cada 1 dezena .
    outro pulando de 4 em 4 anos de 1004 até 2024

--- JS-DanielTapias/ex022fazer uma linha a cada 10 no loop/fazerumalinhaacadadezana.js
+++ JS-DanielTapias/ex022fazer uma linha a cada 10 no loop/fazerumalinhaacadadezana.js
@@ -1,2 +1,8 @@
 let n = parseInt(prompt("digite um numero"))
 
+for(let i = 1; i <= 1000; i++){
+    document.write(n + "x" + i + ": " + n * i + "<br>")
+    if(i % 10 === 0){
+        document.write("<hr>")
+    }
+}
---
+++ JS-DanielTapias/ex023mostrar na tela 4 em 4 anos 1004 até 2024/mostrarnatelaosanos.js
@@ -0,0 +1,8 @@
+let i = 0
+
+
+for( let ano = 1004; ano <= 2024; ano += 4 ){
+    i++
+    document.write(ano + "<br>"  )
+}
+console.log(i)
