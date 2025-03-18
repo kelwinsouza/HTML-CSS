ec3b05861475eae01422938ef8e07e8ce90c4a11
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-17 21:53:17 -0300

    att

---
+++ JS-DanielTapias/logica de programação/Array/ex028 Arrays e obj/arays.js
@@ -0,0 +1,7 @@
+//let nomes = new Array ( "João", "Maria", "daniel", "Beatriz")// sintax função construtora
+
+let nomes = ["João", "Maria", "daniel", "Beatriz", "kelwin", 'Juliana']// sintax literal
+
+for( let i = 0; i < nomes.length; i++){
+    document.write("Nome: " + nomes[i] + "<br>")
+}
