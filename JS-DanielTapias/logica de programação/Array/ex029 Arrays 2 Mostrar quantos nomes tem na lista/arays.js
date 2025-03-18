0c108cd2046bd1a9e2cfeaf83b57b56322f79636
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-17 22:25:27 -0300

    att

---
+++ JS-DanielTapias/logica de programação/Array/ex029 Arrays 2 Mostrar quantos nomes tem na lista/arays.js
@@ -0,0 +1,8 @@
+let nomes = ["João", "Maria", "Daniel", "Beatriz", "Esther"]
+
+for(let i = 0; i < nomes.length; i++){
+    document.write("nomes:" + nomes[i] + "<br>")
+
+}
+
+document.write("A lista de nomes possui: " + nomes.length + " elementos")
