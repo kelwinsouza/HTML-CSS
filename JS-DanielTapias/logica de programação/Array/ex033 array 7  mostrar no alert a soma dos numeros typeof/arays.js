4a8ddba4dd05ed43a6601f51ff16fdec12156bcf
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-18 22:10:40 -0300

    array

---
+++ JS-DanielTapias/logica de programação/Array/ex033 array 7  mostrar no alert a soma dos numeros typeof/arays.js
@@ -0,0 +1,10 @@
+const numero = [2, 31, 0, 12, 55, 17, 36, 85, 68]
+
+let soma = 0
+
+for(let i = 0; i < numero.length; i++){
+    let n = numero[i]
+
+    soma = soma + n
+}
+alert("A soma de todos os números é: " + soma)
