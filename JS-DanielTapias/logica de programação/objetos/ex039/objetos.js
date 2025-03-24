c0f67f80a1eec0e9ee37fd55c354430128662ee3
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-24 20:36:56 -0300

    carro combustivel

--- JS-DanielTapias/logica de programação/objetos/ex039/objetos.js
+++ JS-DanielTapias/logica de programação/objetos/ex039/objetos.js
@@ -1,5 +1,7 @@
-const caneta = {nome: "caneta", 
-    valor: 2, 
-    quantidade: 5
+let carro = { modelo: "Uno",
+    ano : 2023,
+    km: 10000,
+    combustivel: "gasolina",
+    listrosConsumidos: 625
 }
-document.write("A " +caneta.nome+ " custa " + caneta.valor + "R$ e a " + caneta.quantidade + " unidades em estoque")
+document.write(` o carro ${carro.modelo} ano ${carro.}  `
