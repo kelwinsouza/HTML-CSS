6b322f318a460b197b01edb7ce26f64b31a849e8
kelwin souza santos <kelwin29sk8@gmail.com>
2025-03-25 21:00:02 -0300

    DOM
    pequena manipulação de DOM

---
+++ JS-DanielTapias/javaScript/manipulação de DOM/EX001 DOM/pequenamandeDOM.js
@@ -0,0 +1,8 @@
+let user= document.getElementById("user")
+
+document.getElementById("titulo"). textContent = "titulo alterado"
+const lista = document.getElementById("listaTarefas")
+let novaLista ="Aplicar a vagas de emprego"
+
+lista.innerHTML +=   "<li>Teste</lii>"
+//lista.innerHTML = lista.innerHTML + "<li>teste</lii>"
