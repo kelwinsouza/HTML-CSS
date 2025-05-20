41ab5fc081fb35b66945a30183ab9ed7a9b8c7d2
kelwin souza santos <kelwin29sk8@gmail.com>
2025-05-20 17:26:40 -0300

    att

--- JS-DanielTapias/logica de programação/funcao/EX048/funcao.js
+++ JS-DanielTapias/logica de programação/funcao/EX048/funcao.js
@@ -5,10 +5,15 @@ let tabuadaHTML= ""
 
 function mostrarTabuada() {
     const inputvalue = input.value;
+    let tabuadaHTML = ""; 
 
-    for(let i = 1; i < 100; i++){
+    for(let i = 1; i <= 100; i++){
         tabuadaHTML += `${inputvalue} x ${i} = ${inputvalue * i} <br>`	
     }
     output.innerHTML = tabuadaHTML;
 
+}
+function limparInputs(){
+    output.innerHTML = ""
+    input.value = ""
 }
